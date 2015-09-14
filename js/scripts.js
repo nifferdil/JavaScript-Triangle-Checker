2483a86b3fc4c6688216f1eb5135078e24d0678d
perry eising & jennifer morkunas <student@epicodus.com>
2015-09-14 12:00:38 -0700

    specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,41 @@ var side1;
 var side2;
 var side3;
 
-
 var triangleTracker = function(side1, side2, side3) {
-
-  // console.log(side1, side2, side3);
-
 
   if ((side1 + side2 >= side3) && (side3 + side2 >= side1) && (side1 + side3 >= side2)) {
     return true;
   } else {
     return false;
   }
-
 };
+
+var isEquilateral = function (side1, side2, side3){
+  if (side1 == side2 && side2 == side3){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+var isIsosceles = function (side1, side2, side3){
+  if ((side1 == side2) || (side1 == side3) || (side2 == side3)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+var isScalene = function (side1, side2, side3){
+  if (side1 != side2 && side1 != side3 && side2 != side3){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 $(document).ready(function() {
   $("form#triangle-tracker").submit(function(event) {
@@ -29,10 +51,26 @@ $(document).ready(function() {
     $(".triangle").text(side1 + ", " +  side2 + ", " + side3 );
 
     if (!result) {
-      $(".not").text("does not form");
+      $(".not").text("does not form a triangle.");
     }
+
     else {
-      $(".not").text("does form");
+
+      var resultEquilateral = isEquilateral (side1, side2, side3);
+      var resultIsosceles = isIsosceles (side1, side2, side3);
+      var resultScalene = isScalene (side1, side2, side3);
+
+      if (resultEquilateral){
+        $(".not").text("forms an Equilateral triangle.");
+      }
+
+      else if (resultIsosceles){
+        $(".not").text("forms an Isosceles triangle.");
+      }
+
+      else {
+        $(".not").text("forms a Scalene triangle.");
+      }
     }
 
     $("#result").show();
--- spec/specs.js
+++ spec/specs.js
@@ -2,17 +2,16 @@ describe('triangleTracker', function() {
   it("is true for a triangle where the sum of two sides equals the third", function() {
     expect(triangleTracker(4, 4, 8)).to.equal(true);
   });
-  //
-  // it("is true for most years divisible by 4", function() {
-  //   expect(leapYear(2012)).to.equal(true);
-  // });
-  //
-  // it("is false for most years divisible by 100", function() {
-  //   expect(leapYear(1900)).to.equal(false);
-  // });
-  //
-  // it("is false for most years divisible by 400", function() {
-  //   expect(leapYear(1600)).to.equal(true);
-  // });
 
+  it("is true for a triangle where all three sides are of equal length", function() {
+    expect(isEquilateral(4, 4, 4)).to.equal(true);
+  });
+
+  it("is true for a triangle where two sides are of equal length", function() {
+    expect(isIsosceles(4, 4, 2)).to.equal(true);
+  });
+
+  it("is true for a triangle where all three sides are not of equal length", function() {
+    expect(isScalene(4, 3, 2)).to.equal(true);
+  });
 });
